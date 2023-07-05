0b7155921fbb814d0ee339b8ed2d021cd4b9060c
Richard <Richard@wanchain.org>
2023-07-05 17:13:37 +0800

    update env

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,4 +4,16 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
+  starknet: {
+    network: "myNetwork"
+
+  },
+  networks: {
+    "imyNetwork":{
+      dockerizedVersion: "0.8.1",
+      // venv: "<VENV_PATH>",
+      // cairo1BinDir: "path/to/compiler/target/release/",
+      url: "https://alpha4.starknet.io",
+    }
+  }
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,25 +4,26 @@
 // You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
-const hre = require("hardhat");
+const starknet = require("hardhat").starknet;
+const PRIVATE_KEY = "0x35431cc164b7174e5daf4305a6cd39a9d2932c053f0f98a5cc56553be261111" 
+accountAddress = "0x2ef44ba5c93dd2d16bb055d62a547af1ceab59a6ae88dac1b20eb0cbebdd1e4"
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
-
-  const lockedAmount = hre.ethers.parseEther("0.001");
-
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
+  const account = await starknet.OpenZeppelinAccount.getAccountFromAddress(
+    accountAddress,
+    PRIVATE_KEY
   );
+  const contractFactory = await starknet.getContractFactory("HelloStarknet");
+  // will call declare version 2 if contract is cairo 1
+  const txHash = await account.declare(contractFactory);  // class declaration
+  const classHash = await contractFactory.getClassHash();
+
+  // const constructorArgs = { initial_balance: 0 };
+  // const options = { maxFee: ... };
+  // implicitly invokes UDC
+  //const contract = await account.deploy(contractFactory, constructorArgs, options);
+  const contract = await account.deploy(contractFactory);
+  console.log("contract:", contract)
 }
 
 // We recommend this pattern to be able to use async/await everywhere
